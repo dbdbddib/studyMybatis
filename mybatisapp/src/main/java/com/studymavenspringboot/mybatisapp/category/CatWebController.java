5b2f72d39b8999bcf5bfd9405fed014a46d58106
dbdbddib <qudgns4475@gmail.com>
2024-07-26 17:16:22 +0900

    0726
    0726

--- mybatisapp/src/main/java/com/studymavenspringboot/mybatisapp/category/CatWebController.java
+++ mybatisapp/src/main/java/com/studymavenspringboot/mybatisapp/category/CatWebController.java
@@ -2,11 +2,67 @@ package com.studymavenspringboot.mybatisapp.category;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/catweb")
 public class CatWebController {
+    @Autowired
+    private CategoryServiceImpl categoryService;
+
+    @GetMapping("")
+    public String indexHome() {
+        return "index";
+    }
+
+    @GetMapping("/html/category_list")    // 브라우저의 URL 주소
+    public String categoryOld(Model model, @RequestParam String name, @RequestParam int page) {
+        try {
+            if (name == null) {
+                name = "";
+            }
+//            List<ICategory> allList = this.categoryService.getAllList();
+            SearchCategoryDto searchCategoryDto = SearchCategoryDto.builder()
+                    .name(name).page(page).build();
+            int count = this.categoryService.countAllByNameContains(searchCategoryDto);
+            searchCategoryDto.setTotal(count);
+            List<ICategory> allList = this.categoryService.findAllByNameContains(searchCategoryDto);
+            model.addAttribute("allList", allList);
+            model.addAttribute("searchCategoryDto", searchCategoryDto);
+        } catch (Exception ex) {
+            log.error(ex.toString());
+            model.addAttribute("error_message", "오류가 발생했습니다. 관리자에게 문의하세요.");
+            return "error/error_save";  // resources/templates 폴더안의 화면파일
+        }
+
+        return "html/category_list";
+    }
+    @GetMapping("/html/category_insert")    // 브라우저의 URL 주소
+    public String categoryOldView(Model model, @RequestParam Long id) {
+        try {
+            if (id == null || id <= 0) {
+                model.addAttribute("error_message", "id는 1보다 커야 합니다.");
+                return "error/error_bad";  // resources/templates 폴더안의 화면파일
+            }
+            ICategory find = this.categoryService.findById(id);
+            if (find == null) {
+                model.addAttribute("error_message", id + " 데이터가 없습니다.");
+                return "error/error_find";
+            }
+            this.categoryService.remove(id);
+        } catch (Exception ex) {
+            log.error(ex.toString());
+            model.addAttribute("error_message", "서버 에러입니다. 관리자에게 문의 하세요.");
+            return "error/error_save";  // resources/templates 폴더안의 화면파일
+        }
+
+        return "html/category_insert";  // resources/templates 폴더안의 화면파일
+    }
+
 }
